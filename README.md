d92cbaaed39975bb3f3aa64168904366a31a7510
Jiří Špác <capajj@gmail.com>
2015-04-20 02:42:02 +0100

    fixed a wrong names for public app sources which had .js extension, now bundling html files as well as js files

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 early release, not configurable yet-works only with default JSPM config(jspm_packages folder and config.js and public folder not renamed).
 
-Run with node by calling `node jab-run`
+Run with node by calling `node jab-run`, make sure you have got text plugin for systemJS installed via JSPM.
+It is neede for html files.
--- jab.js
+++ jab.js
@@ -1,10 +1,13 @@
 var glob = require("glob");
 var extend = require('util')._extend;
+require('array-sugar');
 
 module.exports = function jspmAureliaBundler(passedOpts) {
 	var options = {	//default
-		glob: {ignore: ['jspm_packages/**', 'build.js', 'config.js'], root: './public/', cwd: './public/'}
+		globJs: {ignore: ['jspm_packages/**', 'build.js', 'config.js'], root: './public/', cwd: './public/'},
+		globHtml: {ignore: ['jspm_packages/**', 'index.html'], root: './public/', cwd: './public/'}
 	};
+
 	extend(options, passedOpts);
 
 	var jspm = new require('jspm');
@@ -17,22 +20,28 @@ module.exports = function jspmAureliaBundler(passedOpts) {
 		'babel-runtime'
 	];
 
-	var packages = Object.keys(builder.loader.map).filter(function (pckgName){
+	var packages = Object.keys(builder.loader.map).filter(function(pckgName) {
 		return ignorePackages.indexOf(pckgName) === -1;
 	}).join(' + ');
 
-	glob("**/*.js", options.glob, function (er, files) {
-		if (files) {
-			files.forEach(function (file){
-				packages += ' + ' + file;
-			});
-		}
-
-		console.log('bundling ', packages);
-
-		jspm.bundle(packages, 'public/build.js', {minify: true, inject: true}).then(function() {
-			console.log('done');
+	glob("**/*.js", options.globJs, function(er, jsFiles) {
+		jsFiles.forEach(function(file) {
+			packages += ' + ' + file.substr(0, file.length - 3);
 		});
+
+		glob('**/*.html', options.globHtml, function(err, htmlFiles) {
+			htmlFiles.forEach(function(file) {
+				packages += ' + ' + file + '!text';
+			});
+
+			console.log('bundling ', packages);
+
+			jspm.bundle(packages, 'public/build.js', {minify: true, inject: true}).then(function() {
+				console.log('done');
+			});
+		});
+
+
 	});
 };
 
